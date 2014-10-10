3b3d8f4462984ede8d7241b7ef08f0da96315c1e
Daniel Loy <loybert@gmail.com>
2014-10-10 18:51:44 +0200

    clean up spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
 require 'rubygems'
 require 'rspec'
-require 'mocha'
-
 # require 'rspec/rails'
 
 require 'active_support'
@@ -15,52 +13,32 @@ lib_dir = File.join(File.dirname(__FILE__), %w(.. lib constant_cache))
 
 Dir.glob("#{lib_dir}/*.rb").each { |file| require file }
 
-# Spec::Runner.configuration.mock_with :mocha
-# Spec::Runner.configuration.include ConstantCache::SpecHelper
-
-if false
-  RSpec.configure do |c|
-    # declare an exclusion filter
-    # c.filter_run_excluding :broken => true
-    c.filter_run_excluding dev: true
-    c.filter_run_excluding outdated: true
-    # c.filter_run_excluding slow: true
-    # c.filter_run_excluding broken: true
-    c.filter_run_excluding dredd: true # TODO: remove after dredd-integration
-    c.filter_run_excluding tbd: true
-
-    # filter_run is short-form alias for filter_run_including
-    # c.filter_run :focus => true
-    # c.filter_run focus: true
-
-    c.infer_spec_type_from_file_location!
-  end
-end
-
-
 module ConstantCache
   module SpecHelper
     def enable_caching(klass, values = [], additional_options = {})
       return_values = values.empty? ? [] : values.map { |params| klass.new(params) }
-
       allow(klass).to receive(:all) { return_values }
-
-      ##allow(ActiveRecord::Base).to receive(:      return_values = values.empty? ? [] : values.map { |params| klass.new(params) }
-
       klass.caches_constants(additional_options)
     end
   end
 end
 
 
-
-
 RSpec.configure do |config|
   config.include ConstantCache::SpecHelper
+
+  # declare an exclusion filter
+  # config.filter_run_excluding :broken => true
+
+  # filter_run is short-form alias for filter_run_including
+  # config.filter_run :focus => true
+  # config.filter_run focus: true
 end
 
+# TODO: check for better init
 ActiveRecord::Base.send(:include, ConstantCache::CacheMethods::InstanceMethods)
 ActiveRecord::Base.send(:extend, ConstantCache::CacheMethods::ClassMethods)
+
 
 class BaseClass < ActiveRecord::Base
   def self.columns
