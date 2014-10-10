51eaee781839482ae982608daec6968ac45f2a5b
Daniel Loy <loybert@gmail.com>
2014-10-10 18:52:04 +0200

    fix include

--- lib/constant_cache.rb
+++ lib/constant_cache.rb
@@ -1,11 +1,11 @@
 require 'constant_cache/config'
-require 'constant_cache/baptist'
+require 'constant_cache/helper'
 require 'constant_cache/cache_methods'
 
 module ConstantCache
   # load Railtie if Rails exists
   if defined?(Rails)
     require 'constant_cache/railtie'
-    # CareBert::Railtie.insert
   end
+
 end
