9901e0e3acab74dd5ac5acabc7aa60f970c8a8bd
Daniel Loy <loybert@gmail.com>
2014-10-10 14:38:44 +0200

    Convert specs to RSpec 3.1.3 syntax with Transpec
    This conversion is done by Transpec 2.3.7 with the following command:
        transpec
    
    * 16 conversions
        from: obj.should
          to: expect(obj).to
    
    * 12 conversions
        from: == expected
          to: eq(expected)
    
    * 1 conversion
        from: obj.should_not
          to: expect(obj).not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/examples/constant_cache/cache_methods_spec.rb
+++ spec/examples/constant_cache/cache_methods_spec.rb
@@ -6,50 +6,50 @@ describe SimpleClass, "with constant_cache mix-in" do
 
   it "should set some default options" do
     enable_caching(SimpleClass)
-    SimpleClass.cache_options.should == {:key => :name, :limit => 64}
+    expect(SimpleClass.cache_options).to eq({:key => :name, :limit => 64})
   end
   
   it "should overwrite default options when passed new ones" do
     additional_options = {:key => :new_key, :limit => 100}
 
     enable_caching(SimpleClass, [], additional_options)
-    SimpleClass.cache_options.should == additional_options
+    expect(SimpleClass.cache_options).to eq(additional_options)
   end
   
   it "should use name as the default constant key" do
     enable_caching(SimpleClass, [{:name => 'one', :value => @value}])
-    SimpleClass::ONE.value.should == @value
+    expect(SimpleClass::ONE.value).to eq(@value)
   end
   
   it "should not set constant when value is nil" do
     proc = lambda { enable_caching(SimpleClass, [{:name => '?', :value => @pony}]) }
-    proc.should_not change(SimpleClass.constants, :size)
+    expect(proc).not_to change(SimpleClass.constants, :size)
   end
   
   it "should raise an exception with message when encountering a duplicate constant" do
     proc = lambda { enable_caching(SimpleClass, [{:name => 'duplicate'}, {:name => 'duplicate'}]) }
-    proc.should raise_error(ConstantCache::DuplicateConstantError, 'Constant SimpleClass::DUPLICATE has already been defined')
+    expect(proc).to raise_error(ConstantCache::DuplicateConstantError, 'Constant SimpleClass::DUPLICATE has already been defined')
   end
   
   it "should truncate long constant names" do
     enable_caching(SimpleClass, [{:name => 'a' * 65, :value => @value}])
-    SimpleClass.const_get('A' * 64).value.should == @value
+    expect(SimpleClass.const_get('A' * 64).value).to eq(@value)
   end
 
   it "should honor configuration of truncation point" do
     enable_caching(SimpleClass, [{:name => 'abcdef', :value => @value}], {:limit => 3})
-    SimpleClass::ABC.value.should == @value
+    expect(SimpleClass::ABC.value).to eq(@value)
   end
   
   it "should raise an exception when truncated constant names collide" do
     proc = lambda { enable_caching(SimpleClass, [{:name => 'abcd'}, {:name => 'abef'}], {:limit => 2}) }
-    proc.should raise_error(ConstantCache::DuplicateConstantError)
+    expect(proc).to raise_error(ConstantCache::DuplicateConstantError)
   end
   
   it "should raise an exception when an invalid limit is set" do
     limit = 0
     proc = lambda { SimpleClass.caches_constants(:limit => limit) }
-    proc.should raise_error(ConstantCache::InvalidLimitError, "Limit of #{limit} is invalid")
+    expect(proc).to raise_error(ConstantCache::InvalidLimitError, "Limit of #{limit} is invalid")
   end
 end
 
@@ -57,7 +57,7 @@ describe AlternateClass, "with constant_cache mix-in" do
 
   it "should allow override of key" do
     enable_caching(AlternateClass, [{:name2 => 'foo bar', :value => @value}], {:key => :name2})
-    AlternateClass::FOO_BAR.value.should == @value
+    expect(AlternateClass::FOO_BAR.value).to eq(@value)
   end
 
 end
--- spec/examples/constant_cache/core_ext_spec.rb
+++ spec/examples/constant_cache/core_ext_spec.rb
@@ -3,31 +3,31 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe String do
   
   it "should upcase its characters" do
-    'test'.constant_name.should == 'TEST'
+    expect('test'.constant_name).to eq('TEST')
   end
   
   it "should replace whitespace with a single underscore" do
-    "test this \tformat\nplease.".constant_name.should == 'TEST_THIS_FORMAT_PLEASE'
+    expect("test this \tformat\nplease.".constant_name).to eq('TEST_THIS_FORMAT_PLEASE')
   end
   
   it "should remove leading and trailing whitespace" do
-    ' test '.constant_name.should == 'TEST'
+    expect(' test '.constant_name).to eq('TEST')
   end
   
   it "should remove non-word characters" do
-    '!test?'.constant_name.should == 'TEST'
+    expect('!test?'.constant_name).to eq('TEST')
   end
   
   it "should not singularize plural name" do
-    'tests'.constant_name.should == 'TESTS'
+    expect('tests'.constant_name).to eq('TESTS')
   end
   
   it "should return nil when all characters are removed" do
-    '?'.constant_name.should be_nil
+    expect('?'.constant_name).to be_nil
   end
   
   it "should collapse multiple underscores" do
-    'test__me'.constant_name.should == 'TEST_ME'
+    expect('test__me'.constant_name).to eq('TEST_ME')
   end
   
 end
